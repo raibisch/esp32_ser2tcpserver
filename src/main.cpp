2352449cb850cc73b575f98e5c0594794b11bba7
Raibisch <raibisch@gmail.com>
2020-10-13 21:08:26 +0200

    erstes Commit von Zuhause aus

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 
 // ----Juergens Breakout Board mit M8N
-//*
+/*
  #define RXD2 16
  #define TXD2 17
  #define S2BAUD 9600
@@ -8,25 +8,21 @@
 //*/
 
 // -----DLG Board mit F9P
-/*
 #define RXD2 13
 #define TXD2 5
 #define S2BAUD 115200
 // Achtung hier ist TXD auf der internen LED  GPIO-05 !!!
 #define LED 2 // --> macht nichts (da LED auf GPIO-05)
-//*/
 
 #include <WiFi.h>
 #include <WiFiAP.h>
 
-
 // Static IP für connect to Router
-IPAddress local_IP(192, 168, 2, 102);
+IPAddress local_IP(192, 168, 2, 26);
 // Set your Gateway IP address
 IPAddress gateway(192, 168, 2, 1);
 IPAddress subnet(255, 255, 255, 0);
 IPAddress primaryDNS(192, 168, 2, 1);   //optional
-
 
 // ESP als eigener AccessPoint
 // Set these to your desired credentials.
@@ -47,13 +43,13 @@ WiFiServer server(80);
 uint TryToConnect() 
 {
    uint8_t i_timeout = 0;
-   while ( (WiFi.status() != WL_CONNECTED) and (WiFi.status() != WL_CONNECT_FAILED) and (i_timeout < 50) )
+   while ( (WiFi.status() != WL_CONNECTED) and (WiFi.status() != WL_CONNECT_FAILED) and (i_timeout < 20) )
     {
       i_timeout++;
       Serial.println(".");
-      digitalWrite(LED, HIGH);
+      //digitalWrite(LED, HIGH);
       delay(200);
-      digitalWrite(LED, LOW);
+      //digitalWrite(LED, LOW);
       delay(200);
       Serial.print("WiFi.status=");
       Serial.println((uint) WiFi.status());
@@ -81,26 +77,21 @@ void StartRouterConnect()
   {
     Serial.println("STA Failed to configure");
   }
-   
-  /*
+    
   char ssid1[] = "MessNetz";   // your network SSID (name)
   char pass1[] = "An2HFVUj";   // your network password
-  */
 
   char ssid2[] = "SMC4";      // your network SSID (name)
-  char pass2[] = "###ud361309$$$";   // your network password
+  char pass2[] = "";   // your network password
 
   Serial.println("");
   delay(1000);
   bool bConnected = false;
 
- 
-  Serial.print("...try to Connecting to: ");
-
-  WiFi.mode(WIFI_STA);
   // Versuche Verbindung zu DLG-Netz
-  /*
-   Serial.println(ssid1);
+  Serial.print("...try to Connecting to: ");
+  Serial.println(ssid1);
+  WiFi.mode(WIFI_STA);
   delay(300);
   WiFi.begin(ssid1, pass1);
   delay(300);
@@ -108,15 +99,14 @@ void StartRouterConnect()
   {
       bConnected = true;
   }
-  */
 
-  // Versuche Verbindung zu Heimnetz  
+    // Versuche Verbindung zu Heimnetz  
   if (!bConnected)
   {
-    //WiFi.disconnect();
+    WiFi.disconnect();
     delay(500);
-    //WiFi.mode(WIFI_OFF);
-    //delay(100);
+    WiFi.mode(WIFI_OFF);
+    delay(100);
     WiFi.mode(WIFI_STA);
     
     Serial.print("...try to Connecting to: ");
@@ -142,26 +132,28 @@ void StartRouterConnect()
 void setup() 
 {
  // Set pin mode
-  pinMode(LED,OUTPUT);
-  digitalWrite(LED,LOW);
+  //pinMode(LED,OUTPUT);
+  //digitalWrite(LED,LOW);
   Serial.begin(115200);
   delay(1000);
 
-  // verbinde zu Router
-  StartRouterConnect();
+  // ---- Router MESSNETZ oder SMC4 (192.168.2.26) ---
+  // StartRouterConnect();
   
-  // Eigner AP
-  //StartAccessPointMode();
+  // --- Eigener AP                 (192.168.4.1) ---
+  StartAccessPointMode();
 
   WiFi.setTxPower(WIFI_POWER_19_5dBm);
-  // Print local IP address and start server
+  WiFi.setSleep(false);   
+
+  // Print local IP address and start web server
   Serial.println("");
   Serial.println("--------- WiFi connected -------------");
   
   delay(200);
   server.begin();
   delay(200);
-  digitalWrite(LED,HIGH);
+  //digitalWrite(LED,HIGH);
   Serial2.setRxBufferSize(400);
   Serial2.begin(S2BAUD, SERIAL_8N1, RXD2, TXD2);
 }
@@ -173,10 +165,9 @@ void loop()
   {
     
     WiFiClient client = server.available();
-    client.setNoDelay(true); // by JG
+    //client.setNoDelay(true); // by JG
     Serial.print("* client connected...");
     
-    /*
     while(Serial2.available())
     {
       char c = Serial2.read();
@@ -186,30 +177,39 @@ void loop()
       }
       yield();
     }
-    */
-
-
-    digitalWrite(LED,HIGH);
+    
+    //digitalWrite(LED,HIGH);
 
     while (client.connected()) 
     {
       if(Serial2.available())
       {
-        char c = Serial2.read();
+        delay(1);
+        int cnt_ser2tcp = Serial2.readBytesUntil(0x0A,buffer_ser2tcp, sizeof(buffer_ser2tcp));
+       
+        buffer_ser2tcp[cnt_ser2tcp] = 0x0A;
+        client.write(buffer_ser2tcp, cnt_ser2tcp+1);
+        
 
-        char sVal[4] = {0};
-        sprintf(sVal,"%02X ",c);
-        client.print(sVal);
-        yield();
+        // nur zum debuggen (evt. wieder auskommentieren)
+        //Serial.write(buffer_ser2tcp,cnt_ser2tcp+1);
+      }
+      
+      if(client.available())
+      {
+          Serial2.write(client.read());
+         
+          //char c = client.read();
+          //Serial2.write(c);
+          //Serial.print(c);
       }
       yield();
-      
     } // ... client.Connected()
 
     delay(300);
     //close the connection:
     //client.stop();
-    Serial2.flush();
+    //Serial2.flush();
     //digitalWrite(LED,LOW);
     Serial.println("client disonnected");
 
